5d7feb321063547eba5565a5c5645ff6bdd12304
Fran <francisco.manez@upc.edu>
2022-06-30 13:32:24 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,63 @@
-<template>
-  <div id="app">
+<!--div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div-->
+<template lang="pug">
+  #app
+    p {{ msg }}
+    input(v-model="name")
+    input(v-model="lastName")
+    p {{ fullName }}
+
+    button(v-on:click="format") Format
+    p {{ formattedName }}
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data () {
+    return {
+      msg: 'Hola',
+      name: '',
+      lastName: '',
+      formattedName: ''
+    }
+  },
+
+  computed: {
+    fullName () {
+      return this.name + ' ' + this.lastName
+    }
+  },
+
+  watch: {
+    name () {
+      console.log(this.name)
+    }
+  },
+
+  methods: {
+    format () {
+      this.formattedName = this.name.split(' ').join('-').toUpperCase()
+    }
+  },
+
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
+@import "./scss/main.scss";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
